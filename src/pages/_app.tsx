c92f503625c5be566d46f04c50a7629036cf5eb0
Nate Florencio <nflorencio13@gmail.com>
2023-09-24 16:13:04 -0300

    Finished integration with Stripe.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app'
+import Image from 'next/image'
+import { globalStyles } from '../styles/global'
+
+import logoImg from '../assets/logo.svg'
+import { Container, Header } from '../styles/pages/app'
+
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg.src} alt="" width={129.738} height={52} />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
