74490d5a2f93c714fd47b8af2a679324887de911
Nate Florencio <nflorencio13@gmail.com>
2023-09-24 23:29:08 -0300

    Finished module 4.

---
+++ src/pages/api/checkout.ts
@@ -0,0 +1,36 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { stripe } from '../../lib/stripe'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const { priceId } = req.body
+
+  if (req.method !== 'POST') {
+    res.status(405).json({ error: '405 Method Not Allowed' })
+  }
+
+  if (!priceId) {
+    res.status(400).json({ error: 'Bad Request - Price ID missing' })
+  }
+
+  const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`
+  const cancelUrl = `${process.env.NEXT_URL}/`
+
+  const checkoutSession = await stripe.checkout.sessions.create({
+    success_url: successUrl,
+    cancel_url: cancelUrl,
+    mode: 'payment',
+    line_items: [
+      {
+        price: priceId,
+        quantity: 1,
+      },
+    ],
+  })
+
+  return res.status(201).json({
+    checkoutUrl: checkoutSession.url,
+  })
+}
